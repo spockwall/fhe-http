b643f2ad942192079f4f4028f6ee74005fe6c092
ben0024 <ben@sudoresearch.com>
2024-03-12 17:36:32 -0400

    feat: function for read from steam

---
+++ src/utils/file_ctl.rs
@@ -0,0 +1,6 @@
+use std::io::{Read, Result};
+pub fn read_from_stream<T: Read>(mut stream: T) -> Result<String> {
+    let mut contents: String = String::new();
+    stream.read_to_string(&mut contents)?;
+    Ok(contents)
+}
