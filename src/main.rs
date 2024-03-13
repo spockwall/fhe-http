2820deae0d5391cd77b9fc49af93e0f8be705e43
ben0024 <ben@sudoresearch.com>
2024-03-13 14:25:39 -0400

    feat: add example execution of base64 encode and decode

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+use std::fs::File;
+use std::io::Result;
+mod utils;
+
+fn main() -> Result<()> {
+    let file_path: &str = "./examples/request.txt";
+    let file: File = File::open(file_path)?;
+    let contents: String = utils::file_ctl::read_from_stream(file)?;
+
+    // turn content from String to &str
+    let encoded = utils::base64::encode(contents.as_str());
+    let decoded = utils::base64::decode(encoded.as_str());
+
+    println!("Encode: {}\n", encoded);
+    println!("Decode: {}", decoded);
+    Ok(())
+}
