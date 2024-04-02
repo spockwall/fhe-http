2fe01dc3e4c508fd2cb552019f1b20edc06c5031
ben0024 <ben@sudoresearch.com>
2024-04-02 02:15:24 -0400

    feat: add string encrypt and decrypt function

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 use std::fs::File;
 use std::io::Result;
 mod utils;
+use tfhe::{generate_keys, ConfigBuilder};
 
 fn main() -> Result<()> {
+    // create a fhe config builder
+    let config: tfhe::Config = ConfigBuilder::default().build();
+    let (client_key, _) = generate_keys(config);
+
+    // read text from file
     let file_path: &str = "./examples/request.txt";
     let file: File = File::open(file_path)?;
     let contents: String = utils::file_ctl::read_from_stream(file)?;
 
-    // turn content from String to &str
+    //// turn content from String to &str
     let encoded = utils::base64::encode(contents.as_str());
     let decoded = utils::base64::decode(encoded.as_str());
 
     println!("Encode: {}\n", encoded);
     println!("Decode: {}", decoded);
+
+    // encrypt & decrypt the text
+    let res = utils::file_ctl::encrypt_string(&encoded, client_key.clone());
+    let decrypted = utils::file_ctl::decrypt_chunks(res, client_key.clone());
+    println!("Decrypted: {}", decrypted);
     Ok(())
 }
--- src/utils/file_ctl.rs
+++ src/utils/file_ctl.rs
@@ -1,6 +1,35 @@
 use std::io::{Read, Result};
+use tfhe::{prelude::*, ClientKey, FheUint8};
+
 pub fn read_from_stream<T: Read>(mut stream: T) -> Result<String> {
     let mut contents: String = String::new();
     stream.read_to_string(&mut contents)?;
     Ok(contents)
+}
+
+pub fn encrypt_string(input: &str, client_key: ClientKey) -> Vec<FheUint8> {
+    // divide input into 8-bit chunks
+    let mut encrypted_chunks: Vec<FheUint8> = Vec::new();
+    for byte in input.bytes() {
+        let res = FheUint8::try_encrypt(byte, &client_key);
+        let temp = res.unwrap();
+        encrypted_chunks.push(temp);
+    }
+
+    // turn encrypted_chunks into a string
+
+    return encrypted_chunks;
+}
+
+pub fn decrypt_chunks(input: Vec<FheUint8>, client_key: ClientKey) -> String {
+    // decrypt chunks is a empty vector of bytes at the begining
+    let mut decrypted_chunks: Vec<u8> = Vec::new();
+    for chunk in input {
+        let res: u8 = chunk.decrypt(&client_key);
+        decrypted_chunks.push(res);
+    }
+
+    // turn decrypted_chunks into a string
+    let decrypted_string: String = String::from_utf8(decrypted_chunks).unwrap();
+    return decrypted_string;
 }
