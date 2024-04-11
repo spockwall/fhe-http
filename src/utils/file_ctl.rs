98cf5a179052bdd8327c02f8000a47a03813befd
ben0024 <ben@sudoresearch.com>
2024-04-11 17:14:23 -0400

    feat: add mapping method for json data encryption a mapping describe which properties to be encrypted

--- src/utils/file_ctl.rs
+++ src/utils/file_ctl.rs
@@ -1,3 +1,4 @@
+use crate::configs::types::is_supported_type;
 use serde_json;
 use serde_json::Value;
 use std::io::{Read, Result};
@@ -21,4 +22,36 @@ pub fn parse_json(json: &str) -> serde_json::Map<String, Value> {
     let res: Value = serde_json::from_str(&json).unwrap();
     let data = res.as_object().unwrap();
     return data.clone();
+}
+
+pub fn create_fhe_transfrom_mapping(
+    keys: Vec<&str>,
+    types: Vec<&str>,
+) -> serde_json::Map<String, Value> {
+    // keys: keys to the values to be encrypted
+    // types: types to the values to be encrypted
+
+    if keys.len() != types.len() {
+        panic!("keys and types should have the same length");
+    }
+
+    // check if the types are supported
+    for _type in types.iter() {
+        if !is_supported_type(_type) {
+            panic!("{} is not a supported type", _type);
+        }
+    }
+
+    // using serde_json::Map to create a json object,
+    // keys are from keys and values are from types
+    let mut res: serde_json::Map<String, Value> = serde_json::Map::new();
+    for i in 0..keys.len() {
+        let key = keys[i].to_string();
+        let _type = types[i].to_string();
+        let value = serde_json::json!({
+            "type": _type,
+        });
+        res.insert(key, value);
+    }
+    return res;
 }
