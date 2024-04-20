31ebf263741fa6900e98b4d6474d981edae67d71
ben0024 <ben@sudoresearch.com>
2024-04-20 17:41:48 -0400

    feat: add encryptable and decryptable trait for clear expression delete FheSupport

--- src/utils/fhe.rs
+++ src/utils/fhe.rs
@@ -1,103 +1,81 @@
-use crate::configs::fhe_types::FheSupportedType;
-use std::u32;
-
-use tfhe::{prelude::*, ClientKey, Error, FheInt32, FheInt64, FheUint32, FheUint64, FheUint8};
+use crate::configs::json::{FheJsonValue, NorJsonValue};
+use tfhe::{prelude::*, ClientKey, Error, FheInt64, FheUint64, FheUint8};
 
 // Trait that abstracts over the encryption and decryption operations
-pub trait FheSupport<T, U>
-where
-    T: FheSupportedType<T>,
-{
-    fn encrypt(input: T, client_key: &ClientKey) -> Result<Self, Error>
+
+pub trait Encryptable {
+    fn to_i64(&self) -> i64;
+    fn to_u64(&self) -> u64;
+    fn to_string(&self) -> String;
+    fn encrypt(&self, client_key: &ClientKey) -> Result<FheJsonValue, Error>
     where
         Self: Sized;
-    // set constraints That T must be FheSupportedTypes
-
-    fn decrypt(input: U, client_key: &ClientKey) -> T;
 }
 
-impl FheSupport<i64, FheInt64> for FheInt64 {
-    fn encrypt(input: i64, client_key: &ClientKey) -> Result<FheInt64, Error> {
-        FheInt64::try_encrypt(input, client_key)
-    }
-
-    fn decrypt(input: FheInt64, client_key: &ClientKey) -> i64 {
-        input.decrypt(client_key)
-    }
+pub trait Decryptable {
+    fn decrypt(&self, client_key: &ClientKey) -> Result<NorJsonValue, Error>
+    where
+        Self: Sized;
 }
 
-impl FheSupport<u64, FheUint64> for FheUint64 {
-    fn encrypt(input: u64, client_key: &ClientKey) -> Result<FheUint64, Error> {
-        FheUint64::try_encrypt(input, client_key)
-    }
-
-    fn decrypt(input: FheUint64, client_key: &ClientKey) -> u64 {
-        input.decrypt(client_key)
-    }
-}
-
-impl FheSupport<u32, FheUint32> for FheUint32 {
-    fn encrypt(input: u32, client_key: &ClientKey) -> Result<FheUint32, Error> {
-        FheUint32::try_encrypt(input, client_key)
-    }
-
-    fn decrypt(input: FheUint32, client_key: &ClientKey) -> u32 {
-        input.decrypt(client_key)
-    }
-}
-
-impl FheSupport<i32, FheInt32> for FheInt32 {
-    fn encrypt(input: i32, client_key: &ClientKey) -> Result<FheInt32, Error> {
-        FheInt32::try_encrypt(input, client_key)
-    }
-
-    fn decrypt(input: FheInt32, client_key: &ClientKey) -> i32 {
-        input.decrypt(client_key)
-    }
-}
-impl FheSupport<String, Vec<FheUint8>> for Vec<FheUint8> {
-    fn encrypt(input: String, client_key: &ClientKey) -> Result<Vec<FheUint8>, Error> {
-        // divide input into 8-bit chunks
-        let mut encrypted_chunks: Vec<FheUint8> = Vec::new();
-        for byte in input.bytes() {
-            let res = FheUint8::try_encrypt(byte, client_key);
-            let temp = res.unwrap();
-            encrypted_chunks.push(temp);
+impl Encryptable for NorJsonValue {
+    fn to_i64(&self) -> i64 {
+        match self {
+            NorJsonValue::Int64(n) => *n,
+            _ => panic!("unsupported type passed"), // Unsupported
         }
-
-        // turn encrypted_chunks into a string if Ok
-        Ok(encrypted_chunks)
     }
-
-    fn decrypt(input: Vec<FheUint8>, client_key: &ClientKey) -> String {
-        // decrypt chunks is a empty vector of bytes at the begining
-        let mut decrypted_chunks: Vec<u8> = Vec::new();
-        for chunk in input {
-            let res: u8 = chunk.decrypt(client_key);
-            decrypted_chunks.push(res);
+    fn to_u64(&self) -> u64 {
+        match self {
+            NorJsonValue::Uint64(n) => *n,
+            _ => panic!("unsupported type passed"), // Unsupported
         }
-
-        // turn decrypted_chunks into a string
-        let decrypted_string: String = String::from_utf8(decrypted_chunks).unwrap();
-        return decrypted_string;
+    }
+    fn to_string(&self) -> String {
+        match self {
+            NorJsonValue::Int64(n) => n.to_string(),
+            NorJsonValue::Uint64(n) => n.to_string(),
+            NorJsonValue::String(s) => s.clone(),
+        }
+    }
+    fn encrypt(&self, client_key: &ClientKey) -> Result<FheJsonValue, Error> {
+        match self {
+            NorJsonValue::Int64(n) => {
+                FheInt64::try_encrypt(*n, client_key).map(FheJsonValue::FheInt64)
+            }
+            NorJsonValue::Uint64(n) => {
+                FheUint64::try_encrypt(*n, client_key).map(FheJsonValue::FheUint64)
+            }
+            NorJsonValue::String(s) => {
+                let encrypted_chunks: Result<Vec<FheUint8>, _> = s
+                    .bytes()
+                    .map(|byte| FheUint8::try_encrypt(byte, client_key))
+                    .collect();
+                encrypted_chunks.map(FheJsonValue::FheString)
+            }
+        }
     }
 }
 
-// wrap the decrypt function for every impl
-pub fn fhe_encrypt<T, U>(input: T, client_key: &ClientKey) -> Result<U, Error>
-where
-    T: FheSupportedType<T>,
-    U: FheSupport<T, U>,
-{
-    U::encrypt(input, client_key)
-}
+impl Decryptable for FheJsonValue {
+    fn decrypt(&self, client_key: &ClientKey) -> Result<NorJsonValue, Error> {
+        match self {
+            FheJsonValue::FheInt64(n) => Ok(NorJsonValue::Int64(n.decrypt(client_key))),
+            FheJsonValue::FheUint64(n) => Ok(NorJsonValue::Uint64(n.decrypt(client_key))),
+            FheJsonValue::FheString(s) => {
+                // decrypt chunks is a empty vector of bytes at the begining
+                let mut decrypted_chunks: Vec<u8> = Vec::new();
+                for chunk in s {
+                    let res: u8 = chunk.decrypt(client_key);
+                    decrypted_chunks.push(res);
+                }
 
-pub fn fhe_decrypt<T, U>(input: U, client_key: &ClientKey) -> T
-where
-    T: FheSupportedType<T>,
-    U: FheSupport<T, U>,
-{
-    U::decrypt(input, client_key)
+                // turn decrypted_chunks into a string
+                let decrypted_string: String = String::from_utf8(decrypted_chunks).unwrap();
+                return Ok(NorJsonValue::String(decrypted_string));
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -110,12 +88,12 @@ mod fhe_tests {
         let http_response= String::from("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: 123\r\n\r\n<html><body>Hello, World!</body></html>");
         let config: tfhe::Config = ConfigBuilder::default().build();
         let (client_key, _) = generate_keys(config);
-        let res_copy = http_response.clone();
-        let encrypted: Result<Vec<FheUint8>, Error> = fhe_encrypt(res_copy, &client_key);
+        let res_copy = NorJsonValue::String(http_response.clone());
+        let encrypted: Result<FheJsonValue, Error> = res_copy.encrypt(&client_key);
         match encrypted {
             Ok(cipher) => {
-                let decrypted: String = fhe_decrypt(cipher, &client_key);
-                assert_eq!(http_response, decrypted);
+                let decrypted = cipher.decrypt(&client_key).unwrap();
+                assert_eq!(http_response, decrypted.to_string());
             }
             Err(_) => panic!("Failed to encrypt the string"),
         }
@@ -124,32 +102,29 @@ mod fhe_tests {
     fn test_encrypt_and_decrypt_u32() {
         let config: tfhe::Config = ConfigBuilder::default().build();
         let (client_key, _) = generate_keys(config);
-        let input_vec: Vec<u32> = vec![0, 11, 4294967295, 34234];
+        let input_vec: Vec<u64> = vec![0, 11, 18446744073709551615, 34234];
         for input in input_vec {
-            let encrypted: Result<FheUint32, Error> = fhe_encrypt(input, &client_key);
-            match encrypted {
-                Ok(cipher) => {
-                    let decrypted: u32 = fhe_decrypt(cipher, &client_key);
-                    assert_eq!(input, decrypted);
-                }
-                Err(_) => panic!("Failed to encrypt the u32"),
-            }
+            let val = NorJsonValue::Uint64(input);
+            let encrypted = val.encrypt(&client_key).unwrap();
+            let decrypted = encrypted.decrypt(&client_key).unwrap();
+            assert_eq!(input, decrypted.to_u64());
         }
     }
     #[test]
     fn test_encrypt_and_decrypt_i32() {
         let config: tfhe::Config = ConfigBuilder::default().build();
         let (client_key, _) = generate_keys(config);
-        let input_vec: Vec<i32> = vec![0, 11, -2_147_483_648, 2_147_483_647, 34234];
+        let input_vec: Vec<i64> = vec![
+            0,
+            1123,
+            -9_223_372_036_854_775_808,
+            9_223_372_036_854_775_807i64,
+        ];
         for input in input_vec {
-            let encrypted: Result<FheInt32, Error> = fhe_encrypt(input, &client_key);
-            match encrypted {
-                Ok(cipher) => {
-                    let decrypted: i32 = fhe_decrypt(cipher, &client_key);
-                    assert_eq!(input, decrypted);
-                }
-                Err(_) => panic!("Failed to encrypt the i32"),
-            }
+            let val = NorJsonValue::Int64(input);
+            let encrypted = val.encrypt(&client_key).unwrap();
+            let decrypted = encrypted.decrypt(&client_key).unwrap();
+            assert_eq!(input, decrypted.to_i64());
         }
     }
 }
