a878638b131762153ee80dcc6d0ac520338548a9
ben0024 <ben@sudoresearch.com>
2024-04-20 16:14:39 -0400

    feat: add base64 en/decode_vec_u8

--- src/utils/base64.rs
+++ src/utils/base64.rs
@@ -1,6 +1,13 @@
 extern crate base64;
 use base64::{engine::general_purpose::STANDARD, Engine as _};
 
+pub fn encode_vec_u8(input: &Vec<u8>) -> String {
+    return STANDARD.encode(input);
+}
+
+pub fn decode_vec_u8(input: &str) -> Result<Vec<u8>, base64::DecodeError> {
+    return STANDARD.decode(input);
+}
 pub fn encode(input: &str) -> String {
     return STANDARD.encode(input.as_bytes());
 }
