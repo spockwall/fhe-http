b5fb41039b1ab87095a5fb1ae1ee041cdd8dab53
ben0024 <ben@sudoresearch.com>
2024-04-20 18:56:02 -0400

    refactor: move tes_base64 to tests folder

--- src/utils/base64.rs
+++ src/utils/base64.rs
@@ -23,24 +23,3 @@ pub fn decode(input: &str) -> String {
         }
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use base64::DecodeError;
-    #[test]
-    fn encode() {
-        let input: &str = "Hello, World!";
-        let expected: &str = "SGVsbG8sIFdvcmxkIQ==";
-        let result: String = STANDARD.encode(input);
-        assert_eq!(result, expected);
-    }
-
-    #[test]
-    fn decode() {
-        let input: &str = "SGVsbG8sIFdvcmxkIQ==";
-        let expected: &str = "Hello, World!";
-        let result: Result<Vec<u8>, DecodeError> = STANDARD.decode(input);
-        assert_eq!(result.unwrap(), expected.as_bytes());
-    }
-}
