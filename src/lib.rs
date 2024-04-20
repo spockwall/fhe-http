cc9d09363c8966c1fdd689f5fc6aa5677e02beb3
ben0024 <ben@sudoresearch.com>
2024-04-20 18:46:33 -0400

    feat:: publish modules in configs and utils

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,11 @@
-pub mod configs;
-pub mod utils;
+pub mod configs {
+    pub mod fhe_types;
+    pub mod json;
+}
+
+pub mod utils {
+    pub mod base64;
+    pub mod fhe;
+    pub mod file_ctl;
+    pub mod json;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
+mod configs;
+mod utils;
 use std::fs::File;
 use std::io::Result;
-mod utils;
 
 fn main() -> Result<()> {
     // parse the json
     let file_path: &str = "./examples/json_files/user.json";
     let file: File = File::open(file_path)?;
     let contents: String = utils::file_ctl::read_from_stream(file)?;
-    let data = utils::file_ctl::parse_json(&contents);
+    let _ = utils::file_ctl::parse_json(&contents);
 
     Ok(())
 }
