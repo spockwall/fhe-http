13f3f51ebf5d863cfef8ce8991915702d4e91ece
ben0024 <ben@sudoresearch.com>
2024-05-01 03:29:53 -0400

    feat(core): change input param of fhe_ops - input serialized data, instead of FheJsonValue

--- fhe_http_core/src/apis/fhe_ops.rs
+++ fhe_http_core/src/apis/fhe_ops.rs
@@ -1,14 +1,30 @@
 use crate::configs::fhe_types::FheSupportedType;
 use crate::configs::json::FheJsonValue;
 use crate::fhe_traits::computable::Computable;
-pub fn fhe_add(a: &FheJsonValue, b: &FheJsonValue, data_type: &FheSupportedType) -> FheJsonValue {
-    a.add(b, data_type)
+use crate::fhe_traits::encrypted_serialize::EncryptedSerialize;
+
+type CompuationResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+pub fn fhe_add(a: &[u8], b: &[u8], data_type: &str) -> CompuationResult<Vec<u8>> {
+    let a: FheJsonValue = EncryptedSerialize::deserialize(a);
+    let b: FheJsonValue = EncryptedSerialize::deserialize(b);
+    let data_type: FheSupportedType = FheSupportedType::from_str(data_type);
+    let result = a.add(&b, &data_type);
+    Ok(result.serialize())
 }
 
-pub fn fhe_sub(a: &FheJsonValue, b: &FheJsonValue, data_type: &FheSupportedType) -> FheJsonValue {
-    a.sub(b, data_type)
+pub fn fhe_sub(a: &[u8], b: &[u8], data_type: &str) -> CompuationResult<Vec<u8>> {
+    let a: FheJsonValue = EncryptedSerialize::deserialize(a);
+    let b: FheJsonValue = EncryptedSerialize::deserialize(b);
+    let data_type: FheSupportedType = FheSupportedType::from_str(data_type);
+    let result = a.sub(&b, &data_type);
+    Ok(result.serialize())
 }
 
-pub fn fhe_mul(a: &FheJsonValue, b: &FheJsonValue, data_type: &FheSupportedType) -> FheJsonValue {
-    a.mul(b, data_type)
+pub fn fhe_mul(a: &[u8], b: &[u8], data_type: &str) -> CompuationResult<Vec<u8>> {
+    let a: FheJsonValue = EncryptedSerialize::deserialize(a);
+    let b: FheJsonValue = EncryptedSerialize::deserialize(b);
+    let data_type: FheSupportedType = FheSupportedType::from_str(data_type);
+    let result = a.mul(&b, &data_type);
+    Ok(result.serialize())
 }
--- fhe_http_core/src/configs/fhe_types.rs
+++ fhe_http_core/src/configs/fhe_types.rs
@@ -6,11 +6,19 @@ pub enum FheSupportedType {
 
 #[allow(dead_code)]
 impl FheSupportedType {
-    fn as_str(&self) -> &'static str {
+    pub fn as_str(&self) -> &'static str {
         match self {
             FheSupportedType::Int64 => "Int64",
             FheSupportedType::Uint64 => "Uint64",
             FheSupportedType::String => "String",
+        }
+    }
+    pub fn from_str(s: &str) -> Self {
+        match s {
+            "Int64" => FheSupportedType::Int64,
+            "Uint64" => FheSupportedType::Uint64,
+            "String" => FheSupportedType::String,
+            _ => panic!("Unsupported type"),
         }
     }
 }
