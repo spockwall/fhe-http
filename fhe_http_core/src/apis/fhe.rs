2eb6f3e32ea5eb27f247564c5d807a0f7d594d83
ben0024 <ben@sudoresearch.com>
2024-05-03 02:07:05 -0400

    feat(core): change encrypt and decrypt input type - from NorJsonValue to vec<u8>

--- fhe_http_core/src/apis/fhe.rs
+++ fhe_http_core/src/apis/fhe.rs
@@ -1,26 +1,26 @@
-use tfhe::ClientKey;
-
 use crate::configs::json::{FheJsonValue, NorJsonValue};
 use crate::fhe_traits::decryptable::Decryptable;
 use crate::fhe_traits::encryptable::Encryptable;
 use crate::fhe_traits::key_serialize::KeySerialize;
-use crate::fhe_traits::value_serialize::FheJsonValueSerialize;
+use crate::fhe_traits::value_serialize::{FheJsonValueSerialize, NorJsonValueSerialize};
+use tfhe::ClientKey;
 
-pub fn encrypt<T: Encryptable>(value: &NorJsonValue, client_key: &Vec<u8>) -> Vec<u8> {
+pub fn encrypt<T: Encryptable>(value: &Vec<u8>, client_key: &Vec<u8>) -> Vec<u8> {
     let deserialized_key: ClientKey = KeySerialize::deserialize(client_key);
-    let encrypted = value.encrypt(&deserialized_key);
+    let deserailized_val: NorJsonValue = NorJsonValueSerialize::deserialize(value);
+    let encrypted = deserailized_val.encrypt(&deserialized_key);
     match encrypted {
         Ok(encrypted) => encrypted.serialize(),
         Err(_) => panic!("Failed to encrypt the value"),
     }
 }
 
-pub fn decrypt<T: Decryptable>(value: &Vec<u8>, client_key: &Vec<u8>) -> NorJsonValue {
+pub fn decrypt<T: Decryptable>(value: &Vec<u8>, client_key: &Vec<u8>) -> Vec<u8> {
     let deserialized_key: ClientKey = KeySerialize::deserialize(client_key);
-    let deserialized_value: FheJsonValue = FheJsonValueSerialize::deserialize(value);
-    let decrypted = deserialized_value.decrypt(&deserialized_key);
+    let deserialized_val: FheJsonValue = FheJsonValueSerialize::deserialize(value);
+    let decrypted = deserialized_val.decrypt(&deserialized_key);
     match decrypted {
-        Ok(decrypted) => decrypted,
+        Ok(decrypted) => decrypted.serialize(),
         Err(_) => panic!("Failed to decrypt the value"),
     }
 }
