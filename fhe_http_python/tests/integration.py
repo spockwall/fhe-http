1d2d08b4c43fea246bcdd6165e2769b3eb5e0d21
ben0024 <ben@sudoresearch.com>
2024-05-06 00:52:16 -0400

    test(pyo3): test substraction, and split logic

--- fhe_http_python/tests/integration.py
+++ fhe_http_python/tests/integration.py
@@ -1,23 +1,58 @@
 import fhe_http_python as py_fhe
 
 
-def test_integration():
-    key_gen = py_fhe.KeyGenerator(True)
+def generate_keys():
+    key_gen = py_fhe.KeyGenerator(new_keys=True)
     client_key = key_gen.get_client_key()
     server_key = key_gen.get_server_key()
-    key_gen.set_server_key()
+    return client_key, server_key
+
+
+def set_server_key(server_key):
+    key_gen = py_fhe.KeyGenerator(new_keys=False)
+    key_gen.set_server_key(server_key)
+
+
+def encrypt_i64(num: int, client_key):
     fhe_types = py_fhe.FheTypes()
-    fhe = py_fhe.Fhe(bytes(client_key))
+    fhe = py_fhe.Fhe(client_key)
+    return fhe.encrypt(fhe_types.from_i64(num))
+
+
+def decrypt_i64(encrypted_num, client_key):
+    fhe_types = py_fhe.FheTypes()
+    fhe = py_fhe.Fhe(client_key)
+    return fhe_types.to_i64(fhe.decrypt(encrypted_num))
+
+
+def add_i64(a, b, client_key):
+    fhe_types = py_fhe.FheTypes()
+    fhe = py_fhe.Fhe(client_key)
     fhe_ops = py_fhe.FheOps()
-    a = fhe_types.from_i64(5)
-    b = fhe_types.from_i64(6)
-    encrypted_a = fhe.encrypt(bytes(a))
-    encrypted_b = fhe.encrypt(bytes(b))
-
-    encrypted_c = fhe_ops.add(bytes(encrypted_a), bytes(encrypted_b), "Int64")
-    decrypted_c = fhe.decrypt(encrypted_c)
-    c = fhe_types.to_i64(bytes(decrypted_c))
-    print(f"Decrypted C: {c}")
+    encrypted_a = fhe.encrypt(fhe_types.from_i64(a))
+    encrypted_b = fhe.encrypt(fhe_types.from_i64(b))
+    encrypted_c = fhe_ops.add(encrypted_a, encrypted_b, "Int64")
+    return encrypted_c
 
 
-test_integration()
+def sub_i64(a, b, client_key):
+    fhe_types = py_fhe.FheTypes()
+    fhe = py_fhe.Fhe(client_key)
+    fhe_ops = py_fhe.FheOps()
+    encrypted_a = fhe.encrypt(fhe_types.from_i64(a))
+    encrypted_b = fhe.encrypt(fhe_types.from_i64(b))
+    encrypted_c = fhe_ops.sub(encrypted_a, encrypted_b, "Int64")
+    return encrypted_c
+
+
+if __name__ == "__main__":
+    client_key, server_key = generate_keys()
+    set_server_key(server_key)
+    encrypted_a = encrypt_i64(5, client_key)
+    encrypted_b = encrypt_i64(6, client_key)
+    encrypted_c = add_i64(5, 6, client_key)
+    c = decrypt_i64(encrypted_c, client_key)
+    encrypted_d = sub_i64(5, 6, client_key)
+    d = decrypt_i64(encrypted_d, client_key)
+    assert c == 11 and d == -1
+    print("Integration test passed")
