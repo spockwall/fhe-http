10ad5b640e2618b89e8e41c01d4521d07f83cfb6
ben0024 <ben@sudoresearch.com>
2024-05-13 03:19:27 -0400

    feat(core): add server_key and packet checker

--- fhe_http_core/src/apis/http.rs
+++ fhe_http_core/src/apis/http.rs
@@ -1,4 +1,5 @@
 use crate::fhe_traits::key_serialize::KeySerialize;
+use crate::utils::base64;
 use crate::utils::file_ctl;
 use crate::utils::json;
 use tfhe::ClientKey;
@@ -21,4 +22,33 @@ pub fn decrypt_fhe_body(keys: Vec<String>, data: &str, client_key: Vec<u8>) -> S
     let keys = keys.iter().map(|x| x.as_str()).collect();
     let decrypted_body = json::decrypt_json(&keys, &body, &client_key);
     return serde_json::to_string(&decrypted_body).unwrap();
+}
+
+pub fn set_server_key_in_body(server_key: &Vec<u8>, data: &str) -> String {
+    let mut body = file_ctl::parse_json(data);
+    body.insert(
+        "server_key".to_string(),
+        serde_json::Value::String(base64::encode_vec_u8(server_key)),
+    );
+    return serde_json::to_string(&body).unwrap();
+}
+
+pub fn check_http_packet(packet: &str) -> Result<(), &str> {
+    let (header, body) = file_ctl::parse_http_packet(packet);
+    if header == "" || body == "" {
+        return Err("Error parsing HTTP packet");
+    }
+    // check if fhe-method is present in the header
+    let header = file_ctl::parse_json(&header);
+    if !header.contains_key("fhe-method") {
+        return Err("fhe-method not found in the header");
+    }
+
+    // check if server_key is present in the body
+    let body = file_ctl::parse_json(&body);
+    if !body.contains_key("server_key") {
+        return Err("server_key not found in the body");
+    }
+
+    return Ok(());
 }
