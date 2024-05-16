613ce5cf9d048b9fcb1bc126d047a847b51b1a98
ben0024 <ben@sudoresearch.com>
2024-05-15 23:41:10 -0400

    refactor(core): handle error for serialization - in an elegent way

--- fhe_http_core/src/fhe_traits/value_serialize.rs
+++ fhe_http_core/src/fhe_traits/value_serialize.rs
@@ -12,52 +12,32 @@ pub trait FheJsonValueSerialize {
 
 impl FheJsonValueSerialize for FheJsonValue {
     fn serialize(&self) -> Vec<u8> {
-        match self {
-            FheJsonValue::FheInt64(value) => bincode::serialize(&value).unwrap(),
-            FheJsonValue::FheUint64(value) => bincode::serialize(&value).unwrap(),
-            FheJsonValue::FheString(value) => bincode::serialize(&value).unwrap(),
+        match bincode::serialize(&self) {
+            Ok(value) => return value,
+            Err(e) => panic!("Failed to serialize: {}", e),
         }
     }
 
     fn deserialize(data: &Vec<u8>) -> Self {
-        if let Ok(value) = bincode::deserialize::<tfhe::FheInt64>(data) {
-            return FheJsonValue::FheInt64(value);
+        match bincode::deserialize(data) {
+            Ok(value) => return value,
+            Err(e) => panic!("Failed to deserialize: {}", e),
         }
-
-        if let Ok(value) = bincode::deserialize::<tfhe::FheUint64>(data) {
-            return FheJsonValue::FheUint64(value);
-        }
-
-        if let Ok(value) = bincode::deserialize::<Vec<tfhe::FheUint8>>(data) {
-            return FheJsonValue::FheString(value);
-        }
-
-        panic!("Failed to deserialize");
     }
 }
-//
+
 impl NorJsonValueSerialize for NorJsonValue {
     fn serialize(&self) -> Vec<u8> {
-        match self {
-            NorJsonValue::Int64(value) => bincode::serialize(&value).unwrap(),
-            NorJsonValue::Uint64(value) => bincode::serialize(&value).unwrap(),
-            NorJsonValue::String(value) => bincode::serialize(&value).unwrap(),
+        match bincode::serialize(&self) {
+            Ok(value) => return value,
+            Err(e) => panic!("Failed to serialize: {}", e),
         }
     }
 
     fn deserialize(data: &Vec<u8>) -> Self {
-        if let Ok(value) = bincode::deserialize::<i64>(data) {
-            return NorJsonValue::Int64(value);
+        match bincode::deserialize(data) {
+            Ok(value) => return value,
+            Err(e) => panic!("Failed to deserialize: {}", e),
         }
-
-        if let Ok(value) = bincode::deserialize::<u64>(data) {
-            return NorJsonValue::Uint64(value);
-        }
-
-        if let Ok(value) = bincode::deserialize::<String>(data) {
-            return NorJsonValue::String(value);
-        }
-
-        panic!("Failed to deserialize");
     }
 }
