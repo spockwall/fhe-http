f6172382a11991b8e926503a8ea4a6b415f0f5a0
ben0024 <ben@sudoresearch.com>
2024-05-15 23:57:23 -0400

    feat(pyo3): change return type of fhe_ops - pyBytes -> vec<u8>

--- fhe_http_python/src/apis/fhe_ops.rs
+++ fhe_http_python/src/apis/fhe_ops.rs
@@ -2,7 +2,6 @@ use fhe_http_core::apis::fhe_ops::{fhe_add, fhe_mul, fhe_sub};
 use fhe_http_core::fhe_traits::key_serialize::KeySerialize;
 use fhe_http_core::tfhe::{set_server_key, CompressedServerKey, ServerKey};
 use pyo3::prelude::*;
-use pyo3::types::PyBytes;
 
 #[pyclass]
 pub struct FheOps {}
@@ -24,39 +23,18 @@ impl FheOps {
         set_server_key(server_key);
     }
 
-    pub fn add(
-        &self,
-        py: Python<'_>,
-        a: Vec<u8>,
-        b: Vec<u8>,
-        data_type: &str,
-    ) -> PyResult<PyObject> {
-        fhe_add(&a, &b, data_type)
-            .map(|result| PyBytes::new_bound(py, &result).into())
+    pub fn add(&self, a: Vec<u8>, b: Vec<u8>, data_type: &str) -> PyResult<Vec<u8>> {
+        fhe_add(&a, &b, &data_type)
             .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e)))
     }
 
-    pub fn sub(
-        &self,
-        py: Python<'_>,
-        a: Vec<u8>,
-        b: Vec<u8>,
-        data_type: String,
-    ) -> PyResult<PyObject> {
+    pub fn sub(&self, a: Vec<u8>, b: Vec<u8>, data_type: String) -> PyResult<Vec<u8>> {
         fhe_sub(&a, &b, &data_type)
-            .map(|result| PyBytes::new_bound(py, &result).into())
             .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e)))
     }
 
-    pub fn mul(
-        &self,
-        py: Python<'_>,
-        a: Vec<u8>,
-        b: Vec<u8>,
-        data_type: String,
-    ) -> PyResult<PyObject> {
+    pub fn mul(&self, a: Vec<u8>, b: Vec<u8>, data_type: String) -> PyResult<Vec<u8>> {
         fhe_mul(&a, &b, &data_type)
-            .map(|result| PyBytes::new_bound(py, &result).into())
             .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e)))
     }
 }
