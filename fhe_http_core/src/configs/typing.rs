b4661d679ea95662053d086540f0593d3b502eef
ben0024 <ben@sudoresearch.com>
2024-05-19 16:04:12 -0400

    feat(typing): define serial value - keys - values - FheValues - other

--- fhe_http_core/src/configs/typing.rs
+++ fhe_http_core/src/configs/typing.rs
@@ -1,46 +1,19 @@
-use tfhe::{FheInt64, FheUint64, FheUint8};
-
+// key_serializable.rs
 pub type SerialServerKey = Vec<u8>;
 pub type SerialClientKey = Vec<u8>;
-pub type SerialNorJsonValue = Vec<u8>;
-pub type SerialFheJsonValue = Vec<u8>;
+pub type SerialCompressedServerKey = Vec<u8>;
+pub type SerialCompressedCompactPublicKey = Vec<u8>;
+
+// value_serializable.rs
+pub type SerialInt64 = Vec<u8>;
+pub type SerialUint64 = Vec<u8>;
+//pub type SerialString = Vec<u8>;
+
+// fhe_value_serializable.rs
+pub type SerialFheInt64 = Vec<u8>;
+pub type SerialFheUint64 = Vec<u8>;
+//pub type SerialFheString = Vec<u8>;
+
+// other
 pub type StringfiedJson = str;
-
-#[derive(serde::Deserialize, serde::Serialize)]
-pub enum NorJsonValue {
-    Int64(i64),
-    Uint64(u64),
-    String(String),
-}
-
-#[derive(serde::Deserialize, serde::Serialize)]
-pub enum FheJsonValue {
-    FheInt64(FheInt64),
-    FheUint64(FheUint64),
-    FheString(Vec<FheUint8>),
-}
-
-pub enum FheSupportedType {
-    Int64,
-    Uint64,
-    String,
-}
-
-#[allow(dead_code)]
-impl FheSupportedType {
-    pub fn as_str(&self) -> &'static str {
-        match self {
-            FheSupportedType::Int64 => "Int64",
-            FheSupportedType::Uint64 => "Uint64",
-            FheSupportedType::String => "String",
-        }
-    }
-    pub fn from_str(s: &str) -> Self {
-        match s {
-            "Int64" => FheSupportedType::Int64,
-            "Uint64" => FheSupportedType::Uint64,
-            "String" => FheSupportedType::String,
-            _ => panic!("Unsupported type"),
-        }
-    }
-}
+pub type CompuationResult<T> = Result<T, &'static str>;
