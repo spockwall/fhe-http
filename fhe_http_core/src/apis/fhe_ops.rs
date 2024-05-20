67c21742167f95db50b34e39a51d5670d90ac36a
ben0024 <ben@sudoresearch.com>
2024-05-20 03:33:33 -0400

    feat(ops): using macro for fhe operations - computable and shihtable

--- fhe_http_core/src/apis/fhe_ops.rs
+++ fhe_http_core/src/apis/fhe_ops.rs
@@ -1,110 +1,79 @@
-use crate::configs::typing::CompuationResult;
-use crate::fhe_traits::computable::{Computable, Shiftable};
-use crate::fhe_traits::decryptable::Decryptable;
+use crate::configs::typing::{CompuationResult, FheValue};
+use crate::fhe_traits::computable::Computable;
+use crate::fhe_traits::computable::Shiftable;
 use crate::fhe_traits::serializable::FheValueSerializable;
+use tfhe::{FheInt64, FheUint64};
 
-fn perform_binary_operation<T, F>(
-    a: &Vec<u8>,
-    b: &Vec<u8>,
-    operation: F,
-) -> CompuationResult<Vec<u8>>
-where
-    T: Decryptable + FheValueSerializable,
-    F: Fn(&T, &T) -> T,
-{
-    let a: T = FheValueSerializable::try_deserialize(a).unwrap();
-    let b: T = FheValueSerializable::try_deserialize(b).unwrap();
-    let result = operation(&a, &b);
-    Ok(result.try_serialize().expect("Failed to serialize"))
-}
-
-fn perform_unary_operation<T, F>(a: &Vec<u8>, operation: F) -> CompuationResult<Vec<u8>>
-where
-    T: Decryptable + FheValueSerializable,
-    F: Fn(&T) -> T,
-{
-    let a: T = FheValueSerializable::try_deserialize(a).unwrap();
-    let result = operation(&a);
-    Ok(result.try_serialize().expect("Failed to serialize"))
+macro_rules! generate_binary_operation {
+    ($fn_name:ident, $op_trait:ident, $op_method:ident) => {
+        pub fn $fn_name(a: &Vec<u8>, b: &Vec<u8>, data_type: &str) -> CompuationResult<Vec<u8>> {
+            let data_type = FheValue::from_str(data_type);
+            match data_type {
+                FheValue::Int64 => {
+                    let a: FheInt64 = FheValueSerializable::try_deserialize(a).unwrap();
+                    let b: FheInt64 = FheValueSerializable::try_deserialize(b).unwrap();
+                    let result = a.$op_method(&b);
+                    Ok(result.try_serialize().expect("Failed to serialize"))
+                }
+                FheValue::Uint64 => {
+                    let a: FheUint64 = FheValueSerializable::try_deserialize(a).unwrap();
+                    let b: FheUint64 = FheValueSerializable::try_deserialize(b).unwrap();
+                    let result = a.$op_method(&b);
+                    Ok(result.try_serialize().expect("Failed to serialize"))
+                }
+            }
+        }
+    };
 }
 
-pub fn fhe_add<T>(a: &Vec<u8>, b: &Vec<u8>) -> CompuationResult<Vec<u8>>
-where
-    T: Decryptable + FheValueSerializable + Computable,
-{
-    perform_binary_operation(a, b, |a: &T, b: &T| a.add(b))
+macro_rules! generate_unary_operation {
+    ($fn_name:ident, $op_trait:ident, $op_method:ident) => {
+        pub fn $fn_name(a: &Vec<u8>, data_type: &str) -> CompuationResult<Vec<u8>> {
+            let data_type = FheValue::from_str(data_type);
+            match data_type {
+                FheValue::Int64 => {
+                    let a: FheInt64 = FheValueSerializable::try_deserialize(a).unwrap();
+                    let result = a.$op_method();
+                    Ok(result.try_serialize().expect("Failed to serialize"))
+                }
+                FheValue::Uint64 => {
+                    let a: FheUint64 = FheValueSerializable::try_deserialize(a).unwrap();
+                    let result = a.$op_method();
+                    Ok(result.try_serialize().expect("Failed to serialize"))
+                }
+            }
+        }
+    };
 }
 
-pub fn fhe_sub<T>(a: &Vec<u8>, b: &Vec<u8>) -> CompuationResult<Vec<u8>>
-where
-    T: Decryptable + FheValueSerializable + Computable,
-{
-    perform_binary_operation(a, b, |a: &T, b: &T| a.sub(b))
+macro_rules! generate_binary_shift_operation {
+    ($fn_name:ident, $op_trait:ident, $op_method:ident) => {
+        pub fn $fn_name(a: &Vec<u8>, b: &Vec<u8>, data_type: &str) -> CompuationResult<Vec<u8>> {
+            let data_type = FheValue::from_str(data_type);
+            match data_type {
+                FheValue::Uint64 => {
+                    let a: FheUint64 = FheValueSerializable::try_deserialize(a).unwrap();
+                    let b: FheUint64 = FheValueSerializable::try_deserialize(b).unwrap();
+                    let result = a.$op_method(&b);
+                    Ok(result.try_serialize().expect("Failed to serialize"))
+                }
+                _ => {
+                    panic!("Operation is not supported for Int64");
+                }
+            }
+        }
+    };
 }
 
-pub fn fhe_mul<T>(a: &Vec<u8>, b: &Vec<u8>) -> CompuationResult<Vec<u8>>
-where
-    T: Decryptable + FheValueSerializable + Computable,
-{
-    perform_binary_operation(a, b, |a: &T, b: &T| a.mul(b))
-}
-
-pub fn fhe_div<T>(a: &Vec<u8>, b: &Vec<u8>) -> CompuationResult<Vec<u8>>
-where
-    T: Decryptable + FheValueSerializable + Computable,
-{
-    perform_binary_operation(a, b, |a: &T, b: &T| a.div(b))
-}
-pub fn fhe_rem<T>(a: &Vec<u8>, b: &Vec<u8>) -> CompuationResult<Vec<u8>>
-where
-    T: Decryptable + FheValueSerializable + Computable,
-{
-    perform_binary_operation(a, b, |a: &T, b: &T| a.rem(b))
-}
-
-pub fn fhe_and<T>(a: &Vec<u8>, b: &Vec<u8>) -> CompuationResult<Vec<u8>>
-where
-    T: Decryptable + FheValueSerializable + Computable,
-{
-    perform_binary_operation(a, b, |a: &T, b: &T| a.and(b))
-}
-
-pub fn fhe_or<T>(a: &Vec<u8>, b: &Vec<u8>) -> CompuationResult<Vec<u8>>
-where
-    T: Decryptable + FheValueSerializable + Computable,
-{
-    perform_binary_operation(a, b, |a: &T, b: &T| a.or(b))
-}
-
-pub fn fhe_xor<T>(a: &Vec<u8>, b: &Vec<u8>) -> CompuationResult<Vec<u8>>
-where
-    T: Decryptable + FheValueSerializable + Computable,
-{
-    perform_binary_operation(a, b, |a: &T, b: &T| a.xor(b))
-}
-pub fn fhe_shr<T>(a: &Vec<u8>, b: &Vec<u8>) -> CompuationResult<Vec<u8>>
-where
-    T: Decryptable + FheValueSerializable + Shiftable,
-{
-    perform_binary_operation(a, b, |a: &T, b: &T| a.shr(b))
-}
-
-pub fn fhe_shl<T>(a: &Vec<u8>, b: &Vec<u8>) -> CompuationResult<Vec<u8>>
-where
-    T: Decryptable + FheValueSerializable + Shiftable,
-{
-    perform_binary_operation(a, b, |a: &T, b: &T| a.shl(b))
-}
-pub fn fhe_neg<T>(a: &Vec<u8>) -> CompuationResult<Vec<u8>>
-where
-    T: Decryptable + FheValueSerializable + Computable,
-{
-    perform_unary_operation(a, |a: &T| a.neg())
-}
-
-pub fn fhe_not<T>(a: &Vec<u8>) -> CompuationResult<Vec<u8>>
-where
-    T: Decryptable + FheValueSerializable + Computable,
-{
-    perform_unary_operation(a, |a: &T| a.not())
-}
+generate_binary_operation!(fhe_add, Computable, add);
+generate_binary_operation!(fhe_sub, Computable, sub);
+generate_binary_operation!(fhe_mul, Computable, mul);
+generate_binary_operation!(fhe_div, Computable, div);
+generate_binary_operation!(fhe_rem, Computable, rem);
+generate_binary_operation!(fhe_and, Computable, and);
+generate_binary_operation!(fhe_or, Computable, or);
+generate_binary_operation!(fhe_xor, Computable, xor);
+generate_binary_shift_operation!(fhe_shr, Shiftable, shr);
+generate_binary_shift_operation!(fhe_shl, Shiftable, shl);
+generate_unary_operation!(fhe_neg, Computable, neg);
+generate_unary_operation!(fhe_not, Computable, not);
