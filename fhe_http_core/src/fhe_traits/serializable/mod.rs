217083e57d58c8d5cda1c7c07cd947eb059b7f41
ben0024 <ben@sudoresearch.com>
2024-06-21 19:23:43 +0800

    refactor: rename modules

--- fhe_http_core/src/fhe_traits/serializable/mod.rs
+++ fhe_http_core/src/fhe_traits/serializable/mod.rs
@@ -1,10 +1,10 @@
-pub mod fhe_value_serialable;
-pub mod key_serializable;
-pub mod value_serializable;
-pub mod zk_serializable;
+pub mod fhe_value;
+pub mod key;
+pub mod value;
+pub mod zk;
 
 // using prelude to re-export traits
-pub use fhe_value_serialable::FheValueSerializable;
-pub use key_serializable::KeySerializable;
-pub use value_serializable::ValueSerializable;
-pub use zk_serializable::ZkSerializable;
+pub use fhe_value::FheValueSerializable;
+pub use key::KeySerializable;
+pub use value::ValueSerializable;
+pub use zk::ZkSerializable;
