2cebc5a775d81382807278f01c09ce75d4550663
ben0024 <ben@sudoresearch.com>
2024-06-22 14:04:47 +0800

    feat: add serial proven fhe value type alias

--- fhe_http_core/src/configs/typing.rs
+++ fhe_http_core/src/configs/typing.rs
@@ -5,13 +5,17 @@ pub type SerialCompactPublicKey = Vec<u8>;
 pub type SerialCompressedServerKey = Vec<u8>;
 pub type SerialCompressedCompactPublicKey = Vec<u8>;
 
-// value_serializable.rs
+// value
 pub type SerialInt64 = Vec<u8>;
 pub type SerialUint64 = Vec<u8>;
 
-// fhe_value_serializable.rs
+// fhe_value
 pub type SerialFheInt64 = Vec<u8>;
 pub type SerialFheUint64 = Vec<u8>;
+
+// proven_fhe_value
+pub type SerialProvenInt64 = Vec<u8>;
+pub type SerialProvenUint64 = Vec<u8>;
 
 // zk_serializable.rs
 pub type SerialPublicZkParams = Vec<u8>;
