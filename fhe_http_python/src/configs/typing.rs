925e355a52ea540949a1b5588bbf48e8da9631af
ben0024 <ben@sudoresearch.com>
2024-06-22 20:15:25 +0800

    feat(ty): add pyclass of PyProvenFheValue

--- fhe_http_python/src/configs/typing.rs
+++ fhe_http_python/src/configs/typing.rs
@@ -1,10 +1,16 @@
-use fhe_http_core::configs::typing::FheValue;
+use fhe_http_core::configs::typing::{FheValue, ProvenFheValue};
 use pyo3::prelude::*;
 
 #[pyclass]
 #[derive(Clone, Debug)]
 pub struct PyFheValue {
     pub inner: FheValue,
+}
+
+#[pyclass]
+#[derive(Clone, Debug)]
+pub struct PyProvenFheValue {
+    pub inner: ProvenFheValue,
 }
 
 #[pymethods]
@@ -20,7 +26,25 @@ impl PyFheValue {
     }
 }
 
+#[pymethods]
+impl PyProvenFheValue {
+    #[staticmethod]
+    pub fn from_str(s: &str) -> PyResult<Self> {
+        let value = ProvenFheValue::from_str(s);
+        Ok(PyProvenFheValue { inner: value })
+    }
+
+    pub fn as_str(&self) -> &str {
+        self.inner.as_str()
+    }
+}
+
 #[pyfunction]
 pub fn create_fhe_value_type(s: &str) -> PyResult<PyFheValue> {
     PyFheValue::from_str(s)
+}
+
+#[pyfunction]
+pub fn create_proven_fhe_value_type(s: &str) -> PyResult<PyProvenFheValue> {
+    PyProvenFheValue::from_str(s)
 }
