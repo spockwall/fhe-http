2e8da188df8548212d508c0c3c0bfc3c9bb9b72c
ben0024 <ben@sudoresearch.com>
2024-06-22 14:03:36 +0800

    refactor: remove unused var in macro

--- fhe_http_core/src/apis/fhe_ops.rs
+++ fhe_http_core/src/apis/fhe_ops.rs
@@ -5,7 +5,7 @@ use crate::fhe_traits::serializable::FheValueSerializable;
 use tfhe::{FheInt64, FheUint64};
 
 macro_rules! generate_binary_operation {
-    ($fn_name:ident, $op_trait:ident, $op_method:ident) => {
+    ($fn_name:ident, $op_method:ident) => {
         pub fn $fn_name(
             a: &Vec<u8>,
             b: &Vec<u8>,
@@ -13,14 +13,14 @@ macro_rules! generate_binary_operation {
         ) -> CompuationResult<Vec<u8>> {
             match data_type {
                 FheValue::Int64 => {
-                    let a: FheInt64 = FheValueSerializable::try_deserialize(a).unwrap();
-                    let b: FheInt64 = FheValueSerializable::try_deserialize(b).unwrap();
+                    let a = FheInt64::try_deserialize(a).unwrap();
+                    let b = FheInt64::try_deserialize(b).unwrap();
                     let result = a.$op_method(&b);
                     Ok(result.try_serialize().expect("Failed to serialize"))
                 }
                 FheValue::Uint64 => {
-                    let a: FheUint64 = FheValueSerializable::try_deserialize(a).unwrap();
-                    let b: FheUint64 = FheValueSerializable::try_deserialize(b).unwrap();
+                    let a = FheUint64::try_deserialize(a).unwrap();
+                    let b = FheUint64::try_deserialize(b).unwrap();
                     let result = a.$op_method(&b);
                     Ok(result.try_serialize().expect("Failed to serialize"))
                 }
@@ -30,16 +30,16 @@ macro_rules! generate_binary_operation {
 }
 
 macro_rules! generate_unary_operation {
-    ($fn_name:ident, $op_trait:ident, $op_method:ident) => {
+    ($fn_name:ident, $op_method:ident) => {
         pub fn $fn_name(a: &Vec<u8>, data_type: &FheValue) -> CompuationResult<Vec<u8>> {
             match data_type {
                 FheValue::Int64 => {
-                    let a: FheInt64 = FheValueSerializable::try_deserialize(a).unwrap();
+                    let a = FheInt64::try_deserialize(a).unwrap();
                     let result = a.$op_method();
                     Ok(result.try_serialize().expect("Failed to serialize"))
                 }
                 FheValue::Uint64 => {
-                    let a: FheUint64 = FheValueSerializable::try_deserialize(a).unwrap();
+                    let a = FheUint64::try_deserialize(a).unwrap();
                     let result = a.$op_method();
                     Ok(result.try_serialize().expect("Failed to serialize"))
                 }
@@ -49,7 +49,7 @@ macro_rules! generate_unary_operation {
 }
 
 macro_rules! generate_binary_shift_operation {
-    ($fn_name:ident, $op_trait:ident, $op_method:ident) => {
+    ($fn_name:ident, $op_method:ident) => {
         pub fn $fn_name(
             a: &Vec<u8>,
             b: &Vec<u8>,
@@ -70,15 +70,15 @@ macro_rules! generate_binary_shift_operation {
     };
 }
 
-generate_binary_operation!(fhe_add, Computable, add);
-generate_binary_operation!(fhe_sub, Computable, sub);
-generate_binary_operation!(fhe_mul, Computable, mul);
-generate_binary_operation!(fhe_div, Computable, div);
-generate_binary_operation!(fhe_rem, Computable, rem);
-generate_binary_operation!(fhe_and, Computable, and);
-generate_binary_operation!(fhe_or, Computable, or);
-generate_binary_operation!(fhe_xor, Computable, xor);
-generate_binary_shift_operation!(fhe_shr, Shiftable, shr);
-generate_binary_shift_operation!(fhe_shl, Shiftable, shl);
-generate_unary_operation!(fhe_neg, Computable, neg);
-generate_unary_operation!(fhe_not, Computable, not);
+generate_binary_operation!(fhe_add, add);
+generate_binary_operation!(fhe_sub, sub);
+generate_binary_operation!(fhe_mul, mul);
+generate_binary_operation!(fhe_div, div);
+generate_binary_operation!(fhe_rem, rem);
+generate_binary_operation!(fhe_and, and);
+generate_binary_operation!(fhe_or, or);
+generate_binary_operation!(fhe_xor, xor);
+generate_binary_shift_operation!(fhe_shr, shr);
+generate_binary_shift_operation!(fhe_shl, shl);
+generate_unary_operation!(fhe_neg, neg);
+generate_unary_operation!(fhe_not, not);
