20e70897741370870014230abafa69626749cc4b
ben0024 <ben@sudoresearch.com>
2024-07-07 01:55:42 +0800

    feat: add output error

--- fhe_http_core/src/configs/errors.rs
+++ fhe_http_core/src/configs/errors.rs
@@ -7,6 +7,12 @@ pub enum AsmError {
 
     #[error("Execution error: {0}")]
     ExecutionError(String),
+
+    #[error("Register not found: {0}")]
+    RegisterNotFound(String),
+
+    #[error("Output error: {0}")]
+    OutputError(String),
 }
 
 #[derive(Error, Debug)]
