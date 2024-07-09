80d7a5744ee4d35689e0a2996ed5ed9f23613dce
ben0024 <ben@sudoresearch.com>
2024-07-09 17:21:06 +0800

    feat(build): select tfhe feature based on os

--- fhe_http_core/build.rs
+++ fhe_http_core/build.rs
@@ -4,6 +4,30 @@ use std::path::Path;
 use std::process::Command;
 
 fn main() {
+    // Determine the correct feature for tfhe based on os architecture
+    let target = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let os_family = env::var("CARGO_CFG_TARGET_FAMILY").unwrap();
+    let mut tfhe_features = Vec::new();
+
+    match target.as_str() {
+        "x86_64" => {
+            if os_family == "unix" {
+                tfhe_features.push("x86_64-unix");
+            } else if os_family == "windows" {
+                tfhe_features.push("x86_64");
+            }
+        }
+        "aarch64" => {
+            if os_family == "unix" {
+                tfhe_features.push("aarch64-unix");
+            }
+        }
+        _ => {
+            panic!("Unsupported os architecture: {}, build failed!", target);
+        }
+    }
+
+    // Create a file to store the version information
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("tfhe_version.rs");
 
@@ -14,9 +38,8 @@ fn main() {
         .output()
         .expect("Failed to execute cargo metadata");
 
+    // Find the version of the tfhe crate in cargo.lock
     let metadata = String::from_utf8(output.stdout).expect("cargo metadata output not valid UTF-8");
-
-    // Find the version of the tfhe crate
     let mut tfhe_version = "unknown";
     for package in metadata.split("{").skip(1) {
         if package.contains("\"name\":\"tfhe\"") {
