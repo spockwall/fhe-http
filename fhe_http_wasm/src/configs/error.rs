4a0359a0ed7e694d4f7c1fcfb72cfe8d214b15dc
ben0024 <ben@sudoresearch.com>
2024-07-14 02:25:32 +0800

    feat(): add self defined wasm error

---
+++ fhe_http_wasm/src/configs/error.rs
@@ -0,0 +1,25 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum WasmError {
+    #[error("Key loading error: {0}")]
+    KeyLoadingError(String),
+
+    #[error("Key saving error: {0}")]
+    KeySavingError(String),
+
+    #[error("Key generation error: {0}")]
+    KeyGenerationError(String),
+
+    #[error("public zk params generation error: {0}")]
+    PublicZkParamsGenerationError(String),
+
+    #[error("window object getting error: {0}")]
+    WindowError(String),
+
+    #[error("document object getting error: {0}")]
+    DocumentError(String),
+
+    #[error("local storage getting error: {0}")]
+    LocalStorageError(String),
+}
