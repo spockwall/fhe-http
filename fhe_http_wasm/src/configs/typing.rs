865affc7922b0e014168c22084c58404f8edf231
ben0024 <ben@sudoresearch.com>
2024-07-14 00:11:20 +0800

    feat(typing): add wasm fhe type and prove fhe type - impl deref - add creaet type funcion

---
+++ fhe_http_wasm/src/configs/typing.rs
@@ -0,0 +1,64 @@
+use fhe_http_core::configs::typing::{FheType, ProvenFheType};
+use std::ops::Deref;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+#[derive(Clone, Debug)]
+pub struct WasmFheType {
+    inner: FheType,
+}
+
+#[wasm_bindgen]
+pub struct WasmProvenFheType {
+    inner: ProvenFheType,
+}
+
+impl WasmFheType {
+    pub fn as_str(&self) -> &str {
+        self.inner.as_str()
+    }
+
+    pub fn from_str(s: &str) -> Self {
+        WasmFheType {
+            inner: FheType::from_str(s),
+        }
+    }
+}
+
+impl WasmProvenFheType {
+    pub fn as_str(&self) -> &str {
+        self.inner.as_str()
+    }
+
+    pub fn from_str(s: &str) -> Self {
+        WasmProvenFheType {
+            inner: ProvenFheType::from_str(s),
+        }
+    }
+}
+
+impl Deref for WasmFheType {
+    type Target = FheType;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl Deref for WasmProvenFheType {
+    type Target = ProvenFheType;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+#[wasm_bindgen]
+pub fn create_fhe_type(s: &str) -> WasmFheType {
+    WasmFheType::from_str(s)
+}
+
+#[wasm_bindgen]
+pub fn create_proven_fhe_type(s: &str) -> WasmProvenFheType {
+    WasmProvenFheType::from_str(s)
+}
