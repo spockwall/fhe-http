b22629b75296a6e84ffe8c6a61c7b98fd51aa7a4
ben0024 <ben@sudoresearch.com>
2024-07-15 00:48:51 +0800

    feat(lib): export modules to lib

--- fhe_http_wasm/src/lib.rs
+++ fhe_http_wasm/src/lib.rs
@@ -2,11 +2,13 @@ pub mod apis {
     pub mod base64;
     pub mod fhe;
     pub mod fhe_ops;
+    pub mod http;
     pub mod key_generator;
     pub mod proven_fhe_ops;
     pub mod serializer;
     pub mod server_key_setter;
 }
+
 pub mod configs {
     pub mod error;
     pub mod typing;
@@ -14,6 +16,7 @@ pub mod configs {
 }
 
 pub mod utils {
+    pub mod conversion;
     pub mod web;
 }
 pub use crate::configs::typing::create_fhe_type;
