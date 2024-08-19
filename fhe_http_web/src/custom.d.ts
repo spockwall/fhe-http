741d43323b9d665fe7781fc0dc0e7fe436160015
ben0024 <ben@sudoresearch.com>
2024-08-19 23:47:10 +0800

    feat: add custom function for http data transfer - base64 encode decode - fhe header

--- fhe_http_web/src/custom.d.ts
+++ fhe_http_web/src/custom.d.ts
@@ -1,1 +1,9 @@
-export function hello(): string;
+interface FheHeader {
+    "fhe-method": string;
+    "fhe-version": string;
+    "zk-experimental": string;
+}
+
+export function uint8ArrayToBase64(arr: Uint8Array): string;
+export function base64ToUint8Array(base64: String): Uint8Array;
+export function createFheHeader(fheMethod: String, fheVersion: String): FheHeader;
--- fhe_http_web/src/custom.js
+++ fhe_http_web/src/custom.js
@@ -1,3 +1,18 @@
-export function hello() {
-    return "Hello, world!";
+export function uint8ArrayToBase64(arr) {
+    return Buffer.from(arr).toString("base64");
+}
+
+export function base64ToUint8Array(base64String) {
+    const buffer = Buffer.from(base64String, "base64");
+    const uint8Array = new Uint8Array(buffer);
+    return uint8Array;
+}
+
+export function createFheHeader(fheMethod, fheVersion) {
+    const header = {
+        "fhe-method": fheMethod,
+        "fhe-version": fheVersion,
+        "zk-experiment": "true",
+    };
+    return header;
 }
